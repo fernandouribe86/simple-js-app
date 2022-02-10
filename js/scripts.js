491d77f2846ee7af77180bc5f81796d32e03ee12
Fernando A. Uribe <fauribe56@gmail.com>
2022-02-09 18:33:23 -0800

    Updated styles, js, and index
    If applied, this commit will have changed:
    - JS improvements to remove document.write and include event listeners
    - updated styles
    - updated index formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-
-	const pokemonRepository = (function() {
+const pokemonRepository = (function() {
 		const pokemonList = [
 			{
 				name: 'Bulbasaur',
@@ -45,7 +44,7 @@
 		} else{
 			console.log('This input is now allowed.')
 		}
-}; //added typeof parameter
+	}; //added typeof parameter
 
 	function getAll() {
 		return pokemonList;
@@ -56,36 +55,72 @@
 		return result;
 	};
 
+	function addListItem(pokemon){
+		let container1 = document.querySelector('.pokemon-list');
+		let listItem = document.createElement('li');
+		let button = document.createElement('button');
+		button.innerHTML = pokemon.name;
+		button.classList.add('pokedex-button');
+		listItem.appendChild(button);
+		container1.appendChild(listItem);
+		button.addEventListener('click', () => {
+			showDetails(pokemon);
+		});
+	};
+
+	function showDetails(pokemon){
+		console.log(pokemon.name)
+	}
+
 	  return {
 		add: add,
 		search: search,
-		getAll: getAll
+		getAll: getAll,
+		addListItem: addListItem,
+		showDetails: showDetails,
 	 };
-
-
 
 })();
 const pokemonArray = pokemonRepository.getAll();
 console.log(pokemonArray);
 console.log(pokemonRepository.add({name: 'Pikachu', types: ['electric'], height: '.9'}));
 
-
 console.log(pokemonRepository.getAll());
 
 pokemonArray.forEach(function(pokemon){
-	if(pokemon.height > '1'){
-		document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!' + '</p>');
-	} else {document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
-	}
+	pokemonRepository.addListItem(pokemon);
 });
 
-let pokemonSearch = prompt('Search to see if you have caught this pokemon before!');
-console.log(pokemonSearch);
 
-let pokemonResult = pokemonRepository.search(pokemonArray, pokemonSearch);
-console.log(pokemonResult);
+// HIDING OLD/EXPERIMENTAL CODE FOR REFERENCE
 
-if (pokemonResult.length > 0){
-	document.write('<p> Search Result: You have already found this pokemon. </p>')
-}else{document.write('<p> Search Result: You have not found this pokemon yet. </p>')
-};
+// const pokemonSearchTool = document.querySelector('.search-icon');
+// pokemonSearchTool.addEventListener('click', () => {
+// 	searchTextArea();
+// });
+//
+// function searchTextArea(pokemon){
+// 	document.write('<label class="standard-label" for="contact-message">' + 'Search:' + '</label>' + '<textarea id="contact-message" maxlength="240">'+'</textarea>');
+// }
+
+// pokemonArray.forEach(function(pokemon){
+// 	if(pokemon.height > '1'){
+// 		document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!' + '</p>');
+// 	} else {document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
+// 	}
+// });
+
+
+
+//HIDING SEARCH CODE FOR NOW WHILE I LEARN MORE
+
+// let pokemonSearch = prompt('Search to see if you have caught this pokemon before!');
+// console.log(pokemonSearch);
+//
+// let pokemonResult = pokemonRepository.search(pokemonArray, pokemonSearch);
+// console.log(pokemonResult);
+
+// if (pokemonResult.length > 0){
+// 	document.write('<p> Search Result: You have already found this pokemon. </p>')
+// }else{document.write('<p> Search Result: You have not found this pokemon yet. </p>')
+// };
